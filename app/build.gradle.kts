793943b034ef68b1e60a52a65254799d22a56a43
hep-lo <hepyshilo8@gmail.com>
2026-08-30 14:51:25 +0300

    Prepare release 1.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.personal.sleepalarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.5.2"
+        versionCode = 13
+        versionName = "1.5.3"
 
         resourceConfigurations += listOf("ru", "en")
 
