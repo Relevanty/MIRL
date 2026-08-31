8effe1d12ee03d2b26c608e7cd2c8c0500f78972
hep-lo <hepyshilo8@gmail.com>
2026-08-31 22:55:01 +0300

    chore: prepare MIRL 1.5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.personal.sleepalarm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.5.4"
+        versionCode = 15
+        versionName = "1.5.5"
 
         resourceConfigurations += listOf("ru", "en")
 
